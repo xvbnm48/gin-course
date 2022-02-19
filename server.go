d6585ce90c17134e88c0f0f2c50888723c29c85e
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-02-19 21:35:58 +0700

    gin course

---
+++ server.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/test", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "OK",
+		})
+	})
+	server.Run(":8080")
+}
