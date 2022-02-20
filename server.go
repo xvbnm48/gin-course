2f3b5a4889f30ff91f3094867c3475cd6a897756
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-02-20 10:38:39 +0700

    fix error at video-controller

--- server.go
+++ server.go
@@ -1,14 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/xvbnm48/gin-course/controller"
+	"github.com/xvbnm48/gin-course/service"
+)
 
+var (
+	videoService service.VideoService = service.New()
+	videoController controller.VideoController = controller.New(videoService)
+)
 func main() {
 	server := gin.Default()
 
-	server.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "OK",
-		})
+	server.GET("/posts", func(c *gin.Context) {
+		c.JSON(200, videoController.FindAll())
+	})
+	server.POST("/posts", func(c *gin.Context) {
+		c.JSON(200, videoController.Save(c))
 	})
 	server.Run(":8080")
 }
