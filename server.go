b1632d7ccf87cfa380f573442fe71e631f692fa2
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-02-20 23:28:26 +0700

    make a middleware, and make a logger for debugging gin

--- server.go
+++ server.go
@@ -3,15 +3,19 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/xvbnm48/gin-course/controller"
+	middlewares "github.com/xvbnm48/gin-course/middlewares"
 	"github.com/xvbnm48/gin-course/service"
 )
 
 var (
-	videoService service.VideoService = service.New()
+	videoService    service.VideoService       = service.New()
 	videoController controller.VideoController = controller.New(videoService)
 )
+
 func main() {
-	server := gin.Default()
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger())
 
 	server.GET("/posts", func(c *gin.Context) {
 		c.JSON(200, videoController.FindAll())
